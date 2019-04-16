9ce65b4c5ae33900025f72934a1a9e138ad41fc4
jens-daniel-mueller <jens.mueller@io-warnemuende.de>
2019-04-16 22:16:49 +0200

    comparison plots by distance intervals

--- 1_Read_SOCAT_data.R
+++ 1_Read_SOCAT_data.R
@@ -1,25 +1,19 @@
 # Load required libraries -------------------------------------------------
 
 library(tidyverse)
+library(geosphere)
 
 
-# Read header from complete SOCATv6 tsv file  -----------------------------
+# Finnmaid DOI subset -----------------------------------------------------
 
 meta <- read_delim("Data/SOCATv6.tsv", 
                       "\t", escape_double = FALSE, trim_ws = TRUE, 
                       skip = 4, n_max = 5500)
 
-
-
-# Finnmaid DOI subset -----------------------------------------------------
-
-unique( meta$`Platform Name` )
-
 meta <- meta %>% 
   filter(`Platform Name` %in% c("Finnmaid", "VOS Finnpartner"))
 
 Finnmaid_DOI <- meta$`SOCAT DOI`
-
 
 f <- function(x, pos) subset(x, SOCAT_DOI  %in% Finnmaid_DOI)
 df_DOI <- read_tsv_chunked(here::here("/Data", "SOCATv6.tsv"), skip = 5399,
@@ -28,57 +22,26 @@ df_DOI <- read_tsv_chunked(here::here("/Data", "SOCATv6.tsv"), skip = 5399,
 rm(f, meta, Finnmaid_DOI)
 
 
-# Finnmaid Flag E DOI subset -----------------------------------------------------
+# Finnmaid Flag E DOI subset ----------------------------------------------
 
 meta <- read_delim("Data/SOCATv6_FlagE.tsv", 
                    "\t", escape_double = FALSE, trim_ws = TRUE, 
                    skip = 4, n_max = 107)
-
-unique( meta$`Platform Name` )
 
 meta <- meta %>% 
   filter(`Platform Name` %in% c("Finnmaid", "VOS Finnpartner"))
 
 Finnmaid_DOI <- meta$`SOCAT DOI`
 
-
 f <- function(x, pos) subset(x, SOCAT_DOI  %in% Finnmaid_DOI)
 df_DOI_E <- read_tsv_chunked(here::here("/Data", "SOCATv6_FlagE.tsv"),
                              skip = 154,
                        DataFrameCallback$new(f), chunk_size = 100000)
 
-rm(f)
+df_DOI_E <- df_DOI_E %>% 
+  mutate(Expocode = as.character(Expocode))
 
-# f <- function(x, pos) subset(x, gear == 3)
-# read_csv_chunked(readr_example("mtcars.csv"), DataFrameCallback$new(f), chunk_size = 5)
-
-
-# # Central Baltic Sea subset -----------------------------------------------
-# 
-# f_coord <- function(x, pos) subset(x, `longitude [dec.deg.E]` > 11 & `longitude [dec.deg.E]` < 30 &
-#                                `latitude [dec.deg.N]` > 54 & `latitude [dec.deg.N]` < 61)
-# 
-# df_coord <- read_tsv_chunked(here::here("/Data", "SOCATv6.tsv"), skip = 5399,
-#                        DataFrameCallback$new(f_coord), chunk_size = 100000)
-# 
-# df_coord <- df_coord %>% 
-#   filter(yr >= 2003)
-
-
-# SOCATv6 <- read_delim("Data/SOCATv6.tsv", 
-#                       "\t", escape_double = FALSE, col_types = cols(day = col_number(), 
-#                                                                     hh = col_number(), mm = col_number(), 
-#                                                                     mon = col_number(), ss = col_number(), 
-#                                                                     yr = col_number()), trim_ws = TRUE, 
-#                       skip = 5399, n_max = 1000)
-
-names(df_DOI)
-unique(df_DOI$yr)
-unique(df_coord$yr)
-unique(df_DOI$SOCAT_DOI)
-unique(df_coord$SOCAT_DOI)
-
-
+rm(f, meta, Finnmaid_DOI)
 
 # Read local Finnmaid data file -------------------------------------------
 
@@ -92,16 +55,9 @@ FM_local <- FM_local %>%
          sal = Sal,
          SST = Tem,
          fCO2 = pCO2) %>% 
-  mutate(subset = "Bernds Finnmaid data set, locally stored")
+  mutate(dataset = "Bernds Finnmaid data set, locally stored")
 
 # Merge Central Baltic Sea and DOI data set -------------------------------
-
-df_DOI$subset <- "SOCAT: Finnmaid/Finnpartner DOI"
-df_DOI_E$subset <- "SOCAT: Finnmaid/Finnpartner DOI"
-#df_coord$subset <- "SOCAT: Central Baltic coordinates"
-
-df_DOI_E <- df_DOI_E %>% 
-  mutate(Expocode = as.character(Expocode))
 
 df <- bind_rows(df_DOI, df_DOI_E)
 
@@ -110,21 +66,63 @@ df <- df %>%
          "lat" = "latitude [dec.deg.N]",
          "fCO2" = "fCO2rec [uatm]",
          "SST" = "SST [deg.C]") %>% 
-  mutate(date_time = lubridate::ymd_hms(paste(yr, mon, day, hh, mm, ss))) %>% 
-  select(date_time, lon, lat, sal, SST, fCO2, subset)
+  mutate(date_time = lubridate::ymd_hms(paste(yr, mon, day, hh, mm, ss)),
+         dataset = "SOCAT incl Flag E") %>% 
+  select(date_time, lon, lat, sal, SST, fCO2, dataset)
 
 
 # Include also locally stored Finnmaid data -------------------------------
 
 df <- bind_rows(df, FM_local)
+rm(df_DOI, df_DOI_E, FM_local)
 
+# Define distance intervals from Travemuende ------------------------------
 
+df <- df %>% 
+  mutate(dist_Trave = distGeo(cbind(lon, lat), c(10.8605315, 53.9414096))/1e3,
+         dist_Trave = as.numeric(as.character(
+           cut(dist_Trave, seq(0,1500,20),labels = seq(10,1490,20)))))
 
 # Plot Timeseries ---------------------------------------------------------
 
+#i <- unique(df$dist_Trave)[1]
+
+for (i in unique(df$dist_Trave)) {
+
+  df %>% 
+    filter(dist_Trave == i) %>% 
+    ggplot(aes(date_time, SST, col=dataset))+
+    geom_point(size = 0.5)+
+    scale_color_brewer(palette = "Set1", name="")+
+    labs(x="", y="SST / deg C", title = "Comparison of Finnmaid data sets",
+         subtitle = paste("Distance from Travemünde (+/- 10km):",i,"km"))+
+    theme_bw()+
+    scale_x_datetime(date_breaks = "1 year", date_labels = "%Y")+
+    theme(legend.position = "bottom")
+  
+  ggsave(here::here("/Plots/local_vs_SOCAT", paste("SST_SOCATv6_vs_local_",i,"km.jpg", sep="")),
+         width = 15, height = 4, dpi = 300)
+ 
+  df %>% 
+    filter(dist_Trave == i) %>% 
+    ggplot(aes(date_time, SST))+
+    geom_point(size = 0.5)+
+    labs(x="", y="SST / deg C", title = "Comparison of Finnmaid data sets",
+         subtitle = paste("Distance from Travemünde (+/- 10km):",i,"km"))+
+    theme_bw()+
+    scale_x_datetime(date_breaks = "1 year", date_labels = "%Y")+
+    theme(legend.position = "bottom")+
+    facet_wrap(~dataset, ncol=1)
+  
+  ggsave(here::here("/Plots/local_vs_SOCAT", paste("SST_SOCATv6_vs_local_",i,"km_facet.jpg", sep="")),
+         width = 7, height = 4, dpi = 300)
+  
+}
+
+
 df %>% 
   filter(lat > 59, lat < 59.05) %>% 
-  ggplot(aes(date_time, fCO2, col=subset))+
+  ggplot(aes(date_time, fCO2, col=dataset))+
   geom_point()+
   scale_color_brewer(palette = "Set1", name="")+
   labs(x="", y="pCO2 / fCO2", title = "Comparison of Finnmaid data sets",
@@ -139,13 +137,10 @@ ggsave(here::here("/Plots", "SOCATv6_incl_E.jpg"),
 
 
 
+# Safe merged data file ---------------------------------------------------
 
-# df %>% 
-# write_csv(here::here("/Data", "SOCATv6_Finnmaid.csv"))
-
-# meta %>%
-# write_csv(here::here("/Data", "SOCATv6_Finnmaid_meta.csv"))
-
+df %>%
+write_csv(here::here("/Data", "SOCATv6_Finnmaid.csv"))
 
 
 
